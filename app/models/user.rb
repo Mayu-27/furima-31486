a9e929471d2c489bc549cc132bea125d3a7ebfa1
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2020-12-26 20:11:08 +0900

    Userモデルの単体テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates :birth_date, presence: true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
