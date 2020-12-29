3d80198799410bceec1c850dac5906d3140f8654
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2020-12-29 16:54:55 +0900

    ビューとストロングパラメーターの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :encrypted_password, :last_name, :first_name, :kana_last_name,
+                                      keys: [:nickname, :last_name, :first_name, :kana_last_name,
                                              :kana_first_name, :birth_date])
   end
 end
