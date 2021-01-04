462ce71082087775c817a8c8c6ed1d814f367ea7
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-01-04 18:00:59 +0900

    rubocop での修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(set_params)
     if @item.save
@@ -17,10 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
-private
+  private
+
   def set_params
-    params.require(:item).permit(:image, :name, :introduction, :genre_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :shipping_period_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :item_condition_id, :shipping_fee_id, :prefecture_id,
+                                 :shipping_period_id, :price).merge(user_id: current_user.id)
   end
 end
-
-
--- app/models/item.rb
+++ app/models/item.rb
@@ -23,6 +23,5 @@ class Item < ApplicationRecord
     validates :prefecture_id
   end
 
-  validates :price, numericality: {only_integer: true, :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
-
