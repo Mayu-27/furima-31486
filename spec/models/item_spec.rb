0778fb1fd56c3bba523e7fae1f98010ec3c5bb3d
Mayu-27 <mymskr1027@gmail.com>
2021-01-04 22:11:28 +0900

    テストコードとバリデーションの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -64,29 +64,29 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'genre_idが1以上でないと登録できない' do
-        @item.genre_id = '0'
+        @item.genre_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Genre must be other than 0')
+        expect(@item.errors.full_messages).to include('Genre must be other than 1')
       end
       it 'item_condition_idが1以上でないと登録できない' do
-        @item.item_condition_id = '0'
+        @item.item_condition_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item condition must be other than 0')
+        expect(@item.errors.full_messages).to include('Item condition must be other than 1')
       end
       it 'shipping_fee_idが1以上でないと登録できない' do
-        @item.shipping_fee_id = '0'
+        @item.shipping_fee_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Shipping fee must be other than 0')
+        expect(@item.errors.full_messages).to include('Shipping fee must be other than 1')
       end
       it 'shipping_period_idが1以上でないと登録できない' do
-        @item.shipping_period_id = '0'
+        @item.shipping_period_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Shipping period must be other than 0')
+        expect(@item.errors.full_messages).to include('Shipping period must be other than 1')
       end
       it 'prefecture_idが1以上でないと登録できない' do
-        @item.prefecture_id = '0'
+        @item.prefecture_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Prefecture must be other than 0')
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
     end
   end
