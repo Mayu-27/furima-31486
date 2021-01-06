8ed615886e77099fc22ab420541344b3dac2ee6d
Mayu-27 <mymskr1027@gmail.com>
2021-01-06 20:47:45 +0900

    imageカラムのバリデーション追加、ビューの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,12 +7,17 @@ RSpec.describe Item, type: :model do
 
   describe '商品出品機能' do
     context '新規登録できるとき' do
-      it 'name, introduction,genre_id,item_condition_id,prefecture_id,shipping_fee_id,priceが存在すれば登録できる' do
+      it 'image, name, introduction,genre_id,item_condition_id,prefecture_id,shipping_fee_id,priceが存在すれば登録できる' do
         expect(@item).to be_valid
       end
     end
 
     context '新規登録できないとき' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
       it 'nameが空では登録できない' do
         @item.name = nil
         @item.valid?
