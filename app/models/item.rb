69e9eeb1ad7793cb5cece55588081f9cc5d1a5af
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-01-06 20:47:45 +0900

    imageカラムのバリデーション追加、ビューの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :introduction
+    validates :image
   end
 
   with_options presence: true, numericality: { other_than: 1 } do
@@ -24,4 +25,5 @@ class Item < ApplicationRecord
   end
 
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
 end
