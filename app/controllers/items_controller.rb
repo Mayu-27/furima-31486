258e32995697b25e161d582610855028ec7fd2d3
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-01-09 16:09:33 +0900

    コントローラの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, execpt: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
 
   def index
