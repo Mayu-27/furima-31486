5b9bd44e19f46a0d8a2449fa82caa07eedd6059d
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-01-14 00:35:20 +0900

    purchaseモデルのテストコード記述

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase = FactoryBot.build(:purchase)
+  end
+
+  describe '商品購入機能' do
+    context '購入できるとき' do
+      it 'user_id,item_idが存在すれば登録できる' do
+        expect(@purchase).to be_valid
+      end
+    end
+
+    context '購入できないとき' do
+      it 'user_idが空では登録できない' do
+        @purchase.user_id = nil 
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("User must exist")
+      end
+      it 'item_idが空では登録できない' do
+        @purchase.item_id = nil
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Item must exist")
+      end
+    end
+  end
 end
