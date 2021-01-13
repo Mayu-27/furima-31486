ca95d1422e3eeb822490416fc74e2ca15f11bb7d
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-01-13 21:42:59 +0900

    商品購入フォームの実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 #Formオブジェクト
 class Order
   include ActiveModel::Model
-  attr_accessor :card_number, :card_exp_month, :card_exp_year, :card_cvc, :postal_code, :prefecture, :city, :addresses, :building, :phone_number
+  attr_accessor :card_number, :card_exp_month, :card_exp_year, :card_cvc, :postal_code, :prefecture_id, :city, :street_number, :building_name, :phone_number, :user_id, :item_id
 
 
   with_options presence: true do
@@ -9,16 +9,16 @@ class Order
     validates :card_exp_month, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12}
     validates :card_exp_year, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99}
     validates :card_cvc, numericality: {only_integer: true, length: {maximum: 4}}
-    validates :postal_code, format: {with: /\A[0_9]{3}_[0_9]{4}\z/}
-    validates :prefecture, numericality: { other_than: 1 }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
-    validates :addresses
-    validates :building
+    validates :building_name
+    validates :building_name
     validates :phone_number, numericality:  {only_integer: true}
   end
 
   def save
-    @order = Order.create(card_number: card_number, card_exp_month: card_exp_month, card_exp_year: card_exp_year, card_cvc: card_cvc, user_id: user_id)
-    ShippingAddresses.create(postal_code: postal_code, prefecture: prefecture, city: city, building: building, user_id: user.id)
+    @purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_number: street_number, phone_number: phone_number, building_name: building_name, purchase_id: @purchase.id)
   end
 end
