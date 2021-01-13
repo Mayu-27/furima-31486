1dc48211cfc0ddce024793e9555660391c4ad099
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-01-14 00:14:53 +0900

    エラーメッセージの表示

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,6 @@ class Order
   include ActiveModel::Model
   attr_accessor :card_number, :card_exp_month, :card_exp_year, :card_cvc, :postal_code, :prefecture_id, :city, :street_number, :building_name, :phone_number, :user_id, :item_id
 
-
   with_options presence: true do
     validates :card_number, numericality: {only_integer: true}
     validates :card_exp_month, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12}
@@ -12,8 +11,6 @@ class Order
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
-    validates :building_name
-    validates :building_name
     validates :phone_number, numericality:  {only_integer: true}
   end
 
