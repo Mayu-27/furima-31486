732220743a0248dbea1b8ca40c901324c6d40626
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-01-18 20:21:50 +0900

    card.jsの修正
    公開鍵はPAY.JPで変更し、環境変数は開発環境、本番環境ともに新しい公開鍵で再設定済です。

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,8 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
+
   def index
     @order = Order.new
   end
@@ -6,6 +10,7 @@ class PurchasesController < ApplicationController
   def create
     @order = Order.new(set_params)
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else 
@@ -14,10 +19,28 @@ class PurchasesController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def set_params
-    params.require(:order).permit(:card_number, :card_exp_month, :card_exp_year, :card_cvc, :postal_code, :prefecture_id, :city, :street_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :street_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: set_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
 
 
- 
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 
 const pay = () => {
-  Payjp.setPublicKey("pk_test_41ff0408630995e917fd09ce");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   
   form.addEventListener("submit", (e) => {
@@ -11,16 +11,25 @@ const pay = () => {
 
     const card = {
       number: formData.get("order[card_number]"),
-      cvc: formData.get("card_order[card_cvc]"),
+      cvc: formData.get("order[card_cvc]"),
       exp_month: formData.get("order[card_exp_month]"),
       exp_year: `20${formData.get("order[card_exp_year]")}`,
     };
-
+    
     Payjp.createToken(card, (status, response) => {
       if (status == 200){
         const token = response.id;
-        console.log(token);
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type='hidden'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();  
+
     });
   });
 };
